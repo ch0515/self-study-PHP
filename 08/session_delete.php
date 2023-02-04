b0c05d7e5f94395c9d0b4663216cdf6515ed530b
chaemin <w2114@e-mirim.hs.kr>
2023-02-05 02:56:31 +0900

    세션 삭제하기

---
+++ 08/session_delete.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    unset($_SESSION["username"]);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>세션 삭제하기</title>
+</head>
+<body>
+    <ul>
+        <li><?= "userid 세션 : ".$_SESSION["userid"]."<br>" ?></li>
+        <li><?= "username 세션 : ".$_SESSION["username"]."<br>" ?></li>
+        <li><?= "username 세션이 삭제되었습니다 " ?></li>
+    </ul>
+</body>
+</html>
